3951a6f376f7a5dce6de25ef7cab6e5cc3f37a16
Alexander Sidorchuk <a.o.sidorchuk@gmail.com>
2025-03-06 14:45:40 +0300

    first-task: удаление кубиков долгим нажатием

---
+++ FirstTask/Assets/4 - Scripts/Runtime/Game/XR/ScreenSpaceRayPoseDriver.cs
@@ -0,0 +1,86 @@
+﻿using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+namespace Game
+{
+    public class ScreenSpaceRayPoseDriver : MonoBehaviour
+    {
+        [SerializeField] private Camera controllerCamera;
+        [Space]
+        [SerializeField] private XRInputValueReader<int> screenTouchCountInput = new("Screen Touch Count");
+        [SerializeField] private XRInputValueReader<Vector2> touchPositionInput = new("Touch Position");
+        [SerializeField] private XRInputValueReader<Vector2> tapStartPositionInput = new("Tap Start Position");
+        [SerializeField] private XRInputValueReader<Vector2> dragCurrentPositionInput = new("Drag Current Position");
+
+        private Vector2 tapStartPosition;
+
+        protected void OnEnable()
+        {
+            if (controllerCamera == null)
+            {
+                controllerCamera = Camera.main;
+
+                if (controllerCamera == null)
+                {
+                    enabled = false;
+                    return;
+                }
+            }
+
+            touchPositionInput.EnableDirectActionIfModeUsed();
+            tapStartPositionInput.EnableDirectActionIfModeUsed();
+            dragCurrentPositionInput.EnableDirectActionIfModeUsed();
+            screenTouchCountInput.EnableDirectActionIfModeUsed();
+        }
+
+        protected void OnDisable()
+        {
+            touchPositionInput.DisableDirectActionIfModeUsed();
+            tapStartPositionInput.DisableDirectActionIfModeUsed();
+            dragCurrentPositionInput.DisableDirectActionIfModeUsed();
+            screenTouchCountInput.DisableDirectActionIfModeUsed();
+        }
+
+        protected void Update()
+        {
+            var prevTapStartPosition = tapStartPosition;
+            var tapPerformedThisFrame = tapStartPositionInput.TryReadValue(out tapStartPosition) && prevTapStartPosition != tapStartPosition;
+
+            if (!screenTouchCountInput.TryReadValue(out var screenTouchCount) || screenTouchCount != 1)
+            {
+                return;
+            }
+
+            if (touchPositionInput.TryReadValue(out var touchPosition))
+            {
+                ApplyPose(touchPosition);
+                return;
+            }
+
+            if (dragCurrentPositionInput.TryReadValue(out var screenPosition))
+            {
+                ApplyPose(screenPosition);
+                return;
+            }
+
+            if (tapPerformedThisFrame)
+            {
+                ApplyPose(tapStartPosition);
+                return;
+            }
+        }
+
+        private void ApplyPose(Vector2 screenPosition)
+        {
+            var screenToWorldPoint = controllerCamera.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, controllerCamera.nearClipPlane));
+            var directionVector = (screenToWorldPoint - controllerCamera.transform.position).normalized;
+
+            var localPosition = transform.parent != null
+                ? transform.parent.InverseTransformPoint(screenToWorldPoint)
+                : screenToWorldPoint;
+
+            transform.SetLocalPose(new Pose(localPosition, Quaternion.LookRotation(directionVector)));
+        }
+    }
+}
---
+++ FirstTask/Assets/4 - Scripts/Runtime/Game/XR/ScreenSpaceSelectInput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+namespace Game
+{
+    public class ScreenSpaceSelectInput : MonoBehaviour, IXRInputButtonReader
+    {
+        [SerializeField] private XRInputValueReader<int> screenTouchCountInput = new("Screen Touch Count");
+
+        private bool isPerformed;
+        private bool wasPerformedThisFrame;
+        private bool wasCompletedThisFrame;
+
+        protected void Update()
+        {
+            var prevPerformed = isPerformed;
+
+            isPerformed = screenTouchCountInput.TryReadValue(out var count);
+            wasPerformedThisFrame = !prevPerformed && isPerformed;
+            wasCompletedThisFrame = prevPerformed && !isPerformed;
+        }
+
+        public bool ReadIsPerformed()
+        {
+            return isPerformed;
+        }
+
+        public bool ReadWasPerformedThisFrame()
+        {
+            return wasPerformedThisFrame;
+        }
+
+        public bool ReadWasCompletedThisFrame()
+        {
+            return wasCompletedThisFrame;
+        }
+
+        public float ReadValue()
+        {
+            return isPerformed ? 1f : 0f;
+        }
+
+        public bool TryReadValue(out float value)
+        {
+            value = isPerformed ? 1f : 0f;
+            return isPerformed;
+        }
+    }
+}
